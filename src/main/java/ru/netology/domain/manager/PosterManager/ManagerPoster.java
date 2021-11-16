814f68e0cd73c718d908335097e6db9639648ac0
Vladimir Kovalev <boban_envorter@mail.ru>
2021-11-16 20:05:58 +0300

    начало тестов

--- src/main/java/ru/netology/domain/manager/PosterManager/ManagerPoster.java
+++ src/main/java/ru/netology/domain/manager/PosterManager/ManagerPoster.java
@@ -1,11 +1,20 @@
 package ru.netology.domain.manager.PosterManager;
 
-public class AddFilms {
+public class ManagerPoster {
+    int count = 10;
+
+    public ManagerPoster(int count) {
+        this.count = count;
+    }
+
+    public ManagerPoster() {
+    }
+
     private PosterItem[] items = new PosterItem[0];
 
-    public void add(PosterItem item){
+    public void add(PosterItem item) {
         //создание нового массива на единицу больше
-        int lenght = items.length +1;
+        int lenght = items.length + 1;
         PosterItem[] tmp = new PosterItem[lenght];
         //копирование поэлементно
 
@@ -15,10 +24,15 @@ public class AddFilms {
         tmp[lastFilm] = item;
         items = tmp;
     }
-    public PosterItem[] getAll(){
-        if(items.length > count)
+
+    public PosterItem[] getAll() {
+        if (items.length > count) {
+            count = count;
+        } else {
+            count = items.length;
+        }
         PosterItem[] result = new PosterItem[items.length];
-        for(int i = 0; i < result.length; i++){
+        for (int i = 0; i < result.length; i++) {
             int index = items.length - i - 1;
             result[i] = items[index];
         }
@@ -26,3 +40,4 @@ public class AddFilms {
     }
 
 }
+
---
+++ src/test/java/ru/netology/domain/manager/PosterManager/ManagerPosterTest.java
@@ -0,0 +1,30 @@
+package ru.netology.domain.manager.PosterManager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagerPosterTest {
+
+    ManagerPoster manager = new ManagerPoster();
+
+    PosterItem first = new PosterItem (1, 1, "вечные", "драма", false);
+    PosterItem second = new PosterItem(2, 2, "не время умирать", "боевик", false);
+    PosterItem third = new PosterItem(3, 3, "ряд 19", "драма", false);
+    PosterItem fourth = new PosterItem(4, 4, "кощей.начало", "мультфильм", false);
+    PosterItem fifth = new PosterItem(5,5,"веном 2", "ужасы", true);
+    PosterItem sixth = new PosterItem(6,6, "друг на продажу", "мелодрама", false);
+    PosterItem seventh = new PosterItem(7,7,"дюна","боевик", false);
+    PosterItem eighth = new PosterItem(8,8,"вкус жизни", "драма",false);
+    PosterItem ninth = new PosterItem(9,9,"страшные истории для рассказа незнакомцам","ужасы",false);
+    PosterItem ten = new PosterItem(10,10,"хэлоуин убивает", "ужасы",false);
+    PosterItem eleventh = new PosterItem(11,11,"ледяной демон", "ужасы",false);
+    PosterItem twelve = new PosterItem(12,12,"кошмар на улице вязов", "ужасы",false);
+
+
+
+
+
+
+
+}
